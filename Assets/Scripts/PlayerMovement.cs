03b227578d50412e73924e830101347469b784d1
Justin Martinez <jlmart88@mit.edu>
2014-10-06 15:42:49 -0400

    Added PLayer that moves right

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)){
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+		}
+	
+	}
+
+}
