4f81f861d20767d9f55472f4f07b5d89c6281470
Justin Martinez <jlmart88@mit.edu>
2014-10-06 23:57:10 -0400

    Added player movement, rotating, and camera tracking

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 offset = new Vector3(0,0,-10);
+	public float dampTime = 0.5f;
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		//offset = transform.position; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 curPosition = transform.position;
+		Vector3 nextPosition = player.transform.position + offset;
+		transform.position = Vector3.SmoothDamp (curPosition, nextPosition, ref velocity, dampTime);
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float speed;
+	private Vector2 horizontal;
+	private Vector2 vertical;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,9 +14,31 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.D)){
-			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)){
+			horizontal = Vector2.right * speed * Time.deltaTime;
 		}
+		else if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)){
+			horizontal = -Vector2.right * speed * Time.deltaTime;
+		}
+		else {
+			horizontal = Vector2.right * 0f * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)){
+			vertical = Vector2.up * speed * Time.deltaTime;
+		}
+		else if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)){
+			vertical = -Vector2.up * speed * Time.deltaTime;
+		}
+		else {
+			vertical = Vector2.up * 0f * Time.deltaTime;
+		}
+
+		Vector2 sumVector = horizontal + vertical;
+		rigidbody2D.velocity = sumVector;
+		if (sumVector.y != 0 || sumVector.x != 0) {
+						transform.rotation = Quaternion.AngleAxis (Mathf.Atan2 (sumVector.y, sumVector.x) * Mathf.Rad2Deg, Vector3.forward);
+				}
 	
 	}
 
