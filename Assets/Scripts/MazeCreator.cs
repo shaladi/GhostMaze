c3131008fc0b7b0d54a3bba356b4970f09bd4fd7
Jyotishka Biswas <jbiswas@mit.edu>
2014-10-07 23:35:14 -0400

    Writing script to generate mazes.

---
+++ Assets/Scripts/MazeCreator.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeCreator : MonoBehaviour {
+
+	public int width;
+	public int height;
+
+	public class MapSquare {
+		private MapSquare top;
+		private MapSquare left;
+		private MapSquare right;
+		private MapSquare bottom;
+
+		private GameObject topWall;
+		private GameObject leftWall;
+		private GameObject rightWall;
+		private GameObject bottomWall;
+
+		private int x;
+		private int y;
+
+		public MapSquare (int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		void createWall (int dir) {
+			switch (dir) {
+			case 0:
+				this.top = null;
+
+				return;
+			case 1:
+				this.right = null;
+				return;;
+			case 2:
+				this.bottom = null;
+				return;
+			case 3:
+				this.bottom = null;
+				return;
+			default:
+				return;
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		Generate_maze ();
+	
+	}
+
+	void Generate_maze() {
+
+
+
+		for (int i = 0; i < width; ++i) {
+
+		}
+	}
+
+}
