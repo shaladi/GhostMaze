8cc6a3ea8bd0f5c9322544d82b5baea759a70645
John Mofor <moforj@gmail.com>
2014-10-13 01:48:19 -0400

    Camera now bounded to board (no background shown)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,9 @@ public class CameraController : MonoBehaviour {
 
 	public GameObject player;
 	public Vector3 offset = new Vector3(0,0,-10);
-	public float dampTime = 0.5f;
+	public float dampTime = 0.25f;
+	public float boundary_x = 7.95f;
+	public float boundary_y = 14.5f;
 	private Vector3 velocity = Vector3.zero;
 
 	// Use this for initialization
@@ -17,6 +19,8 @@ public class CameraController : MonoBehaviour {
 	void Update () {
 		Vector3 curPosition = transform.position;
 		Vector3 nextPosition = player.transform.position + offset;
+		nextPosition.x = Mathf.Max (-boundary_x, Mathf.Min (nextPosition.x,boundary_x));
+		nextPosition.y = Mathf.Max (-boundary_y, Mathf.Min (nextPosition.y,boundary_y));
 		transform.position = Vector3.SmoothDamp (curPosition, nextPosition, ref velocity, dampTime);
 	}
 }
