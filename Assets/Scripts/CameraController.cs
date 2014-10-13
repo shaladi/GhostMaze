bb940d42d92b25fad3aec9022f47f63de4d81bba
John Mofor <moforj@gmail.com>
2014-10-13 04:35:03 -0400

    Adding Beacons Now supported

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,24 +3,19 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject player;
-	public Vector3 offset = new Vector3(0,0,-10);
-	public float dampTime = 0.25f;
-	public float boundary_x = 7.95f;
-	public float boundary_y = 14.5f;
-	private Vector3 velocity = Vector3.zero;
+    public GameObject player;
+    public Vector3 offset = new Vector3 (0, 0, -10);
+    public float dampTime = 0.25f;
+    public float boundary_x = 7.95f;
+    public float boundary_y = 14.5f;
+    private Vector3 velocity = Vector3.zero;
 
-	// Use this for initialization
-	void Start () {
-		//offset = transform.position; 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 curPosition = transform.position;
-		Vector3 nextPosition = player.transform.position + offset;
-		nextPosition.x = Mathf.Max (-boundary_x, Mathf.Min (nextPosition.x,boundary_x));
-		nextPosition.y = Mathf.Max (-boundary_y, Mathf.Min (nextPosition.y,boundary_y));
-		transform.position = Vector3.SmoothDamp (curPosition, nextPosition, ref velocity, dampTime);
-	}
+    // Update is called once per frame
+    void Update () {
+        Vector3 curPosition = transform.position;
+        Vector3 nextPosition = player.transform.position + offset;
+        nextPosition.x = Mathf.Max (-boundary_x, Mathf.Min (nextPosition.x, boundary_x));
+        nextPosition.y = Mathf.Max (-boundary_y, Mathf.Min (nextPosition.y, boundary_y));
+        transform.position = Vector3.SmoothDamp (curPosition, nextPosition, ref velocity, dampTime);
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,69 +3,93 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
-	private Vector2 horizontal;
-	private Vector2 vertical;
-	public float sanity = 100;
-	private bool sanityOff = false;
-	private bool sanityCheck = false;
-	private float nextCheckTime = 0;
-	private float sanityCooldownTime = 5;
-	private float sanityTime = 2;
-	private float sanityEnd = 0;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//Horizontal Movement Vector
-		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)){
-			horizontal = Vector2.right * speed * Time.deltaTime;
-		}
-		else if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)){
-			horizontal = -Vector2.right * speed * Time.deltaTime;
-		}
-		else {
-			horizontal = Vector2.right * 0f * Time.deltaTime;
-		}
+    // Player location
+    public float speed;
+    private Vector2 horizontal;
+    private Vector2 vertical;
 
-		//Vertical Movement Vector
-		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)){
-			vertical = Vector2.up * speed * Time.deltaTime;
-		}
-		else if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)){
-			vertical = -Vector2.up * speed * Time.deltaTime;
-		}
-		else {
-			vertical = Vector2.up * 0f * Time.deltaTime;
-		}
+    // Sanity Utils
+    public float sanity = 100;
+    private bool sanityOff = false;
+    private bool sanityCheck = false;
+    private float nextCheckTime = 0;
+    private float sanityCooldownTime = 5;
+    private float sanityTime = 2;
+    private float sanityEnd = 0;
 
-		if (!sanityCheck && nextCheckTime < Time.time) {
-			sanityCheck = Random.Range(0,101) > sanity;
-			nextCheckTime = Time.time + sanityCooldownTime;
-		}
+    // Beacon Utils
+    public GameObject beacon;
+    public uint MAX_BEACON_COUNT = 10;
+    private uint current_beacon_count = 0;
+    public float dropRate = 1f;
+    private float nextDrop = 0f;
 
-		//Sanity Random Movement Vector
-		if (sanityCheck && sanityOff){
-			sanityOff = false;
-			sanityEnd = sanityTime + Time.time;
-			rigidbody2D.velocity = Random.insideUnitCircle * speed * Time.deltaTime;
-		}
-		else if (sanityOff){
-			Vector2 sumVector = horizontal + vertical;
-			rigidbody2D.velocity = sumVector;
-			if (sumVector.y != 0 || sumVector.x != 0) {
-				transform.rotation = Quaternion.AngleAxis (Mathf.Atan2 (sumVector.y, sumVector.x) * Mathf.Rad2Deg, Vector3.forward);
-			}
-		}
-		else if (sanityEnd < Time.time){
-			sanityEnd = 0;
-			sanityOff = true;
-			sanityCheck = false;
-		}
-	
-	}
+
+    // Update is called once per frame
+    void Update () {
+
+        /* 
+         * Player movement Logic
+         */
+
+        //Horizontal Movement Vector
+        if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+            horizontal = Vector2.right * speed * Time.deltaTime;
+        } else if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
+            horizontal = -Vector2.right * speed * Time.deltaTime;
+        } else {
+            horizontal = Vector2.right * 0f * Time.deltaTime;
+        }
+
+        //Vertical Movement Vector
+        if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
+            vertical = Vector2.up * speed * Time.deltaTime;
+        } else if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
+            vertical = -Vector2.up * speed * Time.deltaTime;
+        } else {
+            vertical = Vector2.up * 0f * Time.deltaTime;
+        }
+
+        /* 
+         * Sanity Logic 
+         */
+
+        if (!sanityCheck && nextCheckTime < Time.time) {
+            sanityCheck = Random.Range (0, 101) > sanity;
+            nextCheckTime = Time.time + sanityCooldownTime;
+        }
+
+        //Sanity Random Movement Vector
+        if (sanityCheck && sanityOff) {
+            sanityOff = false;
+            sanityEnd = sanityTime + Time.time;
+            rigidbody2D.velocity = Random.insideUnitCircle * speed * Time.deltaTime;
+        } else if (sanityOff) {
+            Vector2 sumVector = horizontal + vertical;
+            rigidbody2D.velocity = sumVector;
+            if (sumVector.y != 0 || sumVector.x != 0) {
+                transform.rotation = Quaternion.AngleAxis (Mathf.Atan2 (sumVector.y, sumVector.x) * Mathf.Rad2Deg, Vector3.forward);
+            }
+        } else if (sanityEnd < Time.time) {
+            sanityEnd = 0;
+            sanityOff = true;
+            sanityCheck = false;
+        }
+
+        /*
+         * Beacon Logic 
+         */
+
+        // Drop a beacon.
+        if ((Input.GetKey (KeyCode.Space) || Input.GetKey (KeyCode.B)) && (Time.time > nextDrop)) {
+            if (current_beacon_count < MAX_BEACON_COUNT){
+                nextDrop = Time.time + dropRate;
+                current_beacon_count++;
+                Instantiate (beacon, transform.position, Quaternion.identity);
+            }else{
+                // TODO(anyone! Please display a text or play a sound to indicate the player is out of beacons.")
+            }
+        }
+    }
 
 }
