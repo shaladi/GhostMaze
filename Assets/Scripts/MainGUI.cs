dd8e5771fcf697606aa208ee72e3bcdb37acef1e
Justin Martinez <jlmart88@mit.edu>
2014-10-15 02:46:29 -0400

    Working on end game screen

--- Assets/Scripts/MainGUI.cs
+++ Assets/Scripts/MainGUI.cs
@@ -6,6 +6,7 @@ public class MainGUI : MonoBehaviour {
 	public Vector2 size = new Vector2(300,300);
 	public PlayerController playerController;
 	public bool gameHasStarted = false;
+	public bool gameHasEnded = false;
 	// Use this for initialization
 	void Start() {
 //		GUI.BeginGroup (new Rect (0, 0, Screen.width, Screen.height));
@@ -15,7 +16,11 @@ public class MainGUI : MonoBehaviour {
 	void OnGUI () {
 		if (!gameHasStarted) {
 			GUI.ModalWindow (0, new Rect (0, 0, Screen.width, Screen.height), SpawnInstructionsWindow, "");
-		} else {
+		} 
+		else if (gameHasEnded){
+			GUI.ModalWindow (0, new Rect (0, 0, Screen.width, Screen.height), SpawnEndGameWindow, "");
+		}
+		else {
 			var centeredStyle = GUI.skin.GetStyle("Label");
 			centeredStyle.alignment = TextAnchor.UpperLeft;
 			GUI.skin.label.fontSize = 12;
@@ -61,6 +66,41 @@ public class MainGUI : MonoBehaviour {
 		if (GUI.Button(new Rect(Screen.width/2 - 100, 600, 200, 30), "Can you escape?")){
 			Time.timeScale = 1; // starts game
 		    gameHasStarted = true;
+		}  
+		
+	}
+	void SpawnEndGameWindow(int windowID) {
+		Time.timeScale = 0; //pauses the game
+		
+		var centeredStyle = GUI.skin.GetStyle("Label");
+		centeredStyle.alignment = TextAnchor.UpperCenter;
+		GUI.Box (new Rect (0, 0, Screen.width, Screen.height), new GUIContent ());
+		
+		GUI.skin.label.fontSize = 40;
+		GUI.Label (new Rect (0, 100, Screen.width, Screen.height), "GHOST MAZE");
+		
+		GUI.skin.label.fontSize = 20;
+		GUI.Label (new Rect (0, 200, Screen.width, Screen.height), 
+		           "You've awoken in the middle of a dark maze with nothing but a lantern.\n" +
+		           "As you explore the maze, you realize you are slowly losing your <color=red><b>Sanity</b></color>!\n" + 
+		           "You sometimes get so scared that you <color=yellow><b>lose control of where you're walking</b></color>!\n" +
+		           "You need to escape the maze quickly before you go completely insane!\n\n" +
+		           "Luckily, you find 10 <color=cyan><b>Beacons</b></color> in your pocket which you can place down\n " +
+		           "to help you remain sane and give you vision of the maze.\n\n" +
+		           "Controls: \n");
+		
+		centeredStyle.alignment = TextAnchor.UpperLeft;
+		GUI.Label (new Rect (290, 410, Screen.width, Screen.height),
+		           "Arrow Keys/WASD : Move\n"+
+		           "              Spacebar : Place a beacon\n"+
+		           "                          P : Pick up a beacon\n" +
+		           "                          Z : Zoom camera out\n"
+		           );
+		
+		GUI.skin.button.fontSize = 20;
+		if (GUI.Button(new Rect(Screen.width/2 - 100, 600, 200, 30), "Can you escape?")){
+			Time.timeScale = 1; // starts game
+			gameHasStarted = true;
 		}  
 		
 	}
