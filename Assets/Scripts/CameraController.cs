23cabd90857a1f86d99c21ac537491808cf1d861
Justin Martinez <jlmart88@mit.edu>
2014-10-14 20:50:18 -0400

    Made camera zoom out when pressing z, considering removing the torus and directional light

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,15 +5,22 @@ public class CameraController : MonoBehaviour {
 
     public GameObject player;
     public Vector3 offset = new Vector3 (0, 0, -10);
+	private Vector3 adjustedOffset;
     public float dampTime = 0.25f;
-    public float boundary_x = 8.1f;
-    public float boundary_y = 14.5f;
+	public float boundary_x = 12.65f; //tested using 4:3 aspect ratio
+	public float boundary_y = 14.5f; //tested using 4:3 aspect ratio
     private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     void Update () {
+		if (Input.GetKey (KeyCode.Z)) {
+			adjustedOffset = offset - (new Vector3(0,0,20));
+		} else {
+			adjustedOffset = offset;
+		}
+
         Vector3 curPosition = transform.position;
-        Vector3 nextPosition = player.transform.position + offset;
+        Vector3 nextPosition = player.transform.position + adjustedOffset;
         nextPosition.x = Mathf.Max (-boundary_x, Mathf.Min (nextPosition.x, boundary_x));
         nextPosition.y = Mathf.Max (-boundary_y, Mathf.Min (nextPosition.y, boundary_y));
         transform.position = Vector3.SmoothDamp (curPosition, nextPosition, ref velocity, dampTime);
