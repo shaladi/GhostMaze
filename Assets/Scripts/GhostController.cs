3b72796e4ef5ed9465f91cd6e22e3c0f9bfdec5b
Jyotishka Biswas <jbiswas@Jyotishkas-MacBook-Pro.local>
2014-10-15 02:26:37 -0400

    Updated ghost controller code.

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -7,18 +7,24 @@ public class GhostController : MonoBehaviour {
 	// Ghost location
 	public Maze maze;
 	public int numCells;
-	public List<MazeCell> hauntedCells;
+	public List<Vector3> hauntedCells;
 	
-	public float speed =  100f;
-	public float refreshRate = 4f;
+	public float speed;
+	public float refreshRate;
 	//    private float nextCommand = 0f;
-	public int currCellIndex = 1;
-	
+	public int currCellIndex;
+
+	private IntVector2[] translations;
+
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
 		
-//		Vector3 vel = hauntedCells[currCellIndex].transform.position - transform.position + new Vector3 (0.0f, 0.0f, 0.5f);
-//		rigidbody2D.velocity = vel.normalized * speed * Time.deltaTime;
+		Vector3 vel = hauntedCells[currCellIndex] - transform.position;
+//		float mag = vel.magnitude;
+//		if (mag <= 0.1) {
+//			currCellIndex = (currCellIndex + 1) % numCells;
+//		}
+		rigidbody2D.velocity = vel.normalized * speed * Time.deltaTime;
 		//
 		////        /* 
 		////         * Ghost movement Logic
@@ -38,41 +44,30 @@ public class GhostController : MonoBehaviour {
 			currCellIndex = (currCellIndex + 1) % numCells;
 		}
 	}
+
+	private Vector3 ToWorldCoordinates (IntVector2 c) {
+		return new Vector3(4*(c.x - maze.size.x * 0.5f + 0.5f), 4*(c.y - maze.size.y * 0.5f + 0.5f), 0.0f);
+	}
 	
 	public void SetInitialCell (MazeCell cell) {
-//		transform.position = cell.transform.position;
-//		IntVector2 coordinates = cell.coordinates;
-//		for (int i = 0; i < numCells; ++i) {
-//			hauntedCells.Add (cell);
-//			MazeCellEdge edge = null;
-//			int dir = Random.Range(0, 3);
-//			bool valid_dir = false;
-//			int retries = 4;
-//			while(!valid_dir && (retries > 0)) {
-//				try {
-//					edge = cell.GetEdge((MazeDirection)dir);
-//					if (edge.otherCell != null)
-//						valid_dir = true;
-//					else {
-//						dir = (dir + 1) % 4;
-//						--retries;
-//					}
-//				} catch (System.Exception) {
-//					--retries;
-//				}
-//			}
-//			if (hauntedCells.IndexOf(edge.otherCell) == -1) {
-//				cell = edge.otherCell;
-//			} else {
-//				--i;
-//			}
-//		}
-//		for (int i = 1; i < hauntedCells.Count; ++i) {
-//			if (hauntedCells[i] == hauntedCells[i-1]) {
-//				hauntedCells.RemoveAt(i);
-//				--i;
-//			}
-//		}
+		IntVector2 coords = cell.coordinates;
+		translations = new IntVector2[4];
+		translations [0] = new IntVector2 (0, 1);
+		translations [1] = new IntVector2 (1, 0);
+		translations [2] = new IntVector2 (0, -1);
+		translations [3] = new IntVector2 (-1, 0);
+		for (int i = 0; i < numCells; ++i) {
+			int dir = Random.Range (0, 4);
+			IntVector2 newCoords = coords + translations[dir];
+			if (maze.ContainsCoordinates(newCoords)) {
+				hauntedCells.Add(ToWorldCoordinates(coords));
+				coords = newCoords;
+			} else {
+				--i;
+			}
+
+		}
+		currCellIndex = 1;
 	}
 	
 }
